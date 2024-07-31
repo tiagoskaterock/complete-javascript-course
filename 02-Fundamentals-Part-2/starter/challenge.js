c24eb082253f50cb82f95d24de454c0ef3a983cf
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-07-31 14:08:57 -0300

    43. Dot vs. Bracket Notation

---
+++ 02-Fundamentals-Part-2/starter/challenge.js
@@ -0,0 +1,12 @@
+const jonas = {
+    firstName: "Jonas",
+    lastName: "Schedtman",
+    age: 35,
+    job: "Teacher",
+    friends: [
+        "Paul", "Michael", "James", "Joanne", "Adrienne"
+    ]
+    
+}
+
+console.log(jonas.firstName + " has " + jonas.friends.length + " friends and his best friend is " + jonas.friends[0] + ".");
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+const metallica = {
+    vocals: "James",
+    guitar: "Kirk",
+    bass: "Rob",
+    drumms: "Lars"
+}
+
+console.log(metallica)
+console.log(metallica.bass)
+console.log(metallica['guitar'])
+
+
+const favouriteMetallicaAlbum = prompt("What is you favourite Metallica Album?");
+console.log(favouriteMetallicaAlbum)
