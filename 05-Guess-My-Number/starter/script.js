cdc58e78f7b0fefea3ed3f37c3891a83b8ccc8d5
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-09 10:56:11 -0300

    73. Handling Click Events : typeof()

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 document.querySelector(".message").textContent = "Correct Number";
 
 document.querySelector('.number').textContent = '69';
@@ -7,3 +8,11 @@ document.querySelector('.number').textContent = '69';
 document.querySelector(".score").textContent = 11;
 
 document.querySelector('.guess').value = 35;
+*/
+
+const checkBtn = document.querySelector(".check");
+const guess = document.querySelector(".guess");
+
+checkBtn.addEventListener("click", function() {
+    alert(guess.value);
+});
