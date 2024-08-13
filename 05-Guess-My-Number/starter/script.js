7ca15ec3305b8548656316b6f776db32bdd9feee
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-13 20:53:50 -0300

    74. Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,7 +3,7 @@
 const checkBtn = document.querySelector(".check");
 const guess = document.querySelector(".guess");
 const message = document.querySelector(".message");
-const maxNumber = 10;
+const maxNumber = 100;
 const numberToGuess = generateNumber();
 const number = document.querySelector(".number");
 
@@ -15,9 +15,17 @@ checkBtn.addEventListener("click", function() {
     if( ! guess.value) {
         message.textContent = "No guess";
     }
-    else {
-        message.textContent = "Your guess is " + guess.value;
+    else if(parseInt(guess.value) == numberToGuess) {
+        message.textContent = "You guessed it!";
+        checkBtn.computedStyleMap.display = "none";
     }
+    else if(parseInt(guess.value) > numberToGuess){
+        message.textContent = "Your guess is to high";
+        guess.value = "";
+    }
+    else if(guess.value < number){
+        message.textContent = "Your guess is to low";
+        guess.value = "";
+    }
+    
 });
-
-number.textContent = numberToGuess;
