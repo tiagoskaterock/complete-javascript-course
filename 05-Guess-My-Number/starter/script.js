d42654cca7f4e3994749892fea717f773eadd452
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-14 14:12:03 -0300

    74. Implementing the Game Logic -> Game Over Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,6 +6,14 @@ const message = document.querySelector(".message");
 const maxNumber = 100;
 const numberToGuess = generateNumber();
 const number = document.querySelector(".number");
+const score = document.querySelector(".score");
+var scoreValue = parseInt(score.textContent);
+
+function losePoint() {
+    guess.value = "";
+    scoreValue--;
+    score.textContent = scoreValue;
+}
 
 function generateNumber() {
     return Math.trunc(Math.random() * maxNumber + 1);
@@ -21,11 +29,11 @@ checkBtn.addEventListener("click", function() {
     }
     else if(parseInt(guess.value) > numberToGuess){
         message.textContent = "Your guess is to high";
-        guess.value = "";
+        losePoint();
     }
     else if(guess.value < number){
         message.textContent = "Your guess is to low";
-        guess.value = "";
+        losePoint();
     }
     
 });
