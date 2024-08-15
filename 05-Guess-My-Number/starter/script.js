7873e3f5aa7e508b35cdbfdc80760ed014286126
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-15 15:04:28 -0300

    74. Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,16 +3,38 @@
 const checkBtn = document.querySelector(".check");
 const guess = document.querySelector(".guess");
 const message = document.querySelector(".message");
-const maxNumber = 100;
+const maxNumber = 10;
 const numberToGuess = generateNumber();
 const number = document.querySelector(".number");
 const score = document.querySelector(".score");
-var scoreValue = parseInt(score.textContent);
+const pointsLoseForMistake = 1;
+const startingPoints = 3;
+var scoreValue = startingPoints;
 
-function losePoint() {
-    guess.value = "";
-    scoreValue--;
+updatePointsOnScreen();
+setMaxGuess();
+
+function updatePointsOnScreen(){
     score.textContent = scoreValue;
+}
+
+function setMaxGuess() {
+    guess.attributes.max.value = maxNumber;
+}
+
+function losePoint() {    
+    guess.value = "";
+    scoreValue -= pointsLoseForMistake;
+    updatePointsOnScreen();
+    if(scoreValue < 1){        
+        gameOver();
+    }
+}
+
+function gameOver(){
+    scoreValue = 0;
+    updatePointsOnScreen();
+    message.textContent = "GAME OVER";
 }
 
 function generateNumber() {
