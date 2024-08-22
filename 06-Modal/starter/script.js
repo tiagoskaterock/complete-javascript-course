ef132842b0796e615542b7e30d5dd82811d079dd
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-22 17:29:09 -0300

    79. PROJECT #2: Modal Window -> strarting the variables

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const modalElement = document.querySelector('.modal');
+
+document.querySelector('.show-modal').addEventListener('click', function() {
+    modalElement.classList.remove('hidden');
+});
