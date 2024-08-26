8b1b3b163868261aa443a8ccffa91d8f9507ddbd
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-26 14:27:24 -0300

    79. PROJECT #2: Modal Window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,7 +1,35 @@
 'use strict';
 
 const modalElement = document.querySelector('.modal');
+const overlayElement = document.querySelector('.overlay');
+const showModalElements = document.querySelectorAll('.show-modal');
+const closeModalElement = document.querySelector('.close-modal');
 
-document.querySelector('.show-modal').addEventListener('click', function() {
-    modalElement.classList.remove('hidden');
+for (let index = 0; index < showModalElements.length; index++) {
+    console.log(showModalElements[index].textContent);
+    showModalElements[index].addEventListener('click', function() {
+        showModal();
+        showOverlay();
+    })
+}
+
+closeModalElement.addEventListener('click', function() {
+    hideModal();
+    hideOverlay();
 });
+
+function showModal() {
+    modalElement.classList.remove('hidden');
+}
+
+function showOverlay() {
+    overlayElement.classList.remove('hidden');
+}
+
+function hideModal() {
+    modalElement.classList.add('hidden');
+}
+
+function hideOverlay() {
+    overlayElement.classList.add('hidden');
+}
